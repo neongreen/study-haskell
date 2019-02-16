ff9b0ce03e8e3f51129ad254e3b0af5bf000994e
Artyom Kazak <yom@artyom.me>
2019-02-16 12:16:07 +0100

    Widget embedding

--- cms/src/Lib.hs
+++ cms/src/Lib.hs
@@ -13,9 +13,22 @@ module Lib
     ) where
 
 import Servant
-import Lucid
+import Lucid hiding (toHtmlRaw)
 import Servant.HTML.Lucid
 import Network.Wai.Handler.Warp as Warp
+import qualified Data.Text.IO as TIO
+import qualified Data.Text as T
+import Data.Text (Text)
+
+import Data.Foldable
+import Data.String
+import UnliftIO hiding (Handler)
+import System.Directory
+import System.FilePath
+import Control.Monad
+import Data.List (isPrefixOf)
+
+import qualified Lucid
 
 main :: IO ()
 main = Warp.run 9988 app
@@ -30,5 +43,27 @@ api :: Server API
 api = helloWorld
 
 helloWorld :: Handler (Html ())
-helloWorld = pure $
-    p_ "hello world!"
+helloWorld = do
+    widget :: RawHtml <- liftIO $ readAsset "widget.html"
+    pure $ do
+        p_ "Widget goes below:"
+        toHtml widget
+
+----------------------------------------------------------------------------
+-- Raw HTML
+----------------------------------------------------------------------------
+
+newtype RawHtml = RawHtml { unRawHtml :: Text }
+
+readAsset :: FilePath -> IO RawHtml
+readAsset asset = do
+    -- HACKY AND BAD :(
+    path <- makeAbsolute ("assets" </> asset)
+    assets <- makeAbsolute "assets"
+    unless (addTrailingPathSeparator assets `isPrefixOf` path) $
+        error "not allowed"
+    RawHtml <$> TIO.readFile path
+
+instance ToHtml RawHtml where
+    toHtml = Lucid.toHtmlRaw . unRawHtml
+    toHtmlRaw = Lucid.toHtmlRaw . unRawHtml
